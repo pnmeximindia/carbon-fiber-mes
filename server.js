ddaefd48c773e8bcb2a7cc04eada9b4c62c3e25c
pnmeximindia <pnmeximindia@gmail.com>
2026-02-11 02:40:48 +0530

    Add server.js content

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
-server.js
-database.js
-routes.js
-package.json
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Initialize database (creates tables)
+require("./database");
+
+const routes = require("./routes");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// All APIs will start with /api
+app.use("/api", routes);
+
+// Render assigns port dynamically
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("MES running on port", PORT);
+});
